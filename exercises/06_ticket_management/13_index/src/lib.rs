070c517d82d402cbddc74bd0ba528265c48342cb
skyitachi <skbysp@gmail.com>
2024-10-23 08:36:18 +0800

    sync

--- exercises/06_ticket_management/13_index/src/lib.rs
+++ exercises/06_ticket_management/13_index/src/lib.rs
@@ -1,5 +1,6 @@
 // TODO: Implement `Index<&TicketId>` and `Index<TicketId>` for `TicketStore`.
 
+use std::ops::Index;
 use ticket_fields::{TicketDescription, TicketTitle};
 
 #[derive(Clone)]
@@ -10,6 +11,22 @@ pub struct TicketStore {
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct TicketId(u64);
+
+impl Index<TicketId> for TicketStore {
+    type Output = Ticket;
+
+    fn index(&self, index: TicketId) -> &Self::Output {
+        self.get(index).unwrap()
+    }
+}
+
+impl Index<&TicketId> for TicketStore {
+    type Output = Ticket;
+
+    fn index(&self, index: &TicketId) -> &Self::Output {
+        &self[*index]
+    }
+}
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct Ticket {
